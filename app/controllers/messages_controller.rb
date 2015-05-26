b869e22658528865fee7d7e977a39a5f57957603
lczxhf <lczxhf@sina.com>
2015-05-26 13:38:41 +0800

    createclass

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,14 @@
 class MessagesController < ApplicationController
+	require 'net/http'
+  require "rexml/document"
+  include ReplyWeixinMessageHelper
+   skip_before_action :verify_authenticity_token
+   
+   before_action do
+	@wechat_info=WechatInfo.first
+   end
+
+   def receive
+   		puts params
+   end
 end
