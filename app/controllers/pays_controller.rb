fd360dd593c4d4b88167c8b7084fd2b29628bb85
lczxhf <lczxhf@sina.com>
2015-06-02 16:31:54 +0800

    add_pay

---
+++ app/controllers/pays_controller.rb
@@ -0,0 +1,3 @@
+class PaysController < ApplicationController
+	skip_before_action :verify_authenticity_token
+end
---
+++ test/controllers/pays_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PaysControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
